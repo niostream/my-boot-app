69184809515d51dadd000f6b4c8b8ab78da2c105
Urushibara Tomoya <niostream@gmail.com>
2018-10-27 20:14:52 +0900

    リスト3-8とリスト3-9を作成しました。

--- my-boot-app/src/main/java/com/example/HeloController.java
+++ my-boot-app/src/main/java/com/example/HeloController.java
@@ -1,5 +1,24 @@
 package com.example;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HeloController {
+	
+	@RequestMapping("/")
+	public String index() {
+		return "Hello Spring-Boot World!";
+	}
+	
+	@RequestMapping("/{num}")
+	public String index(@PathVariable int num) {
+		int res = 0;
+		for (int i = 1; i <= num; i++) {
+			res += i;
+		}
+		return "total: " + res;
+	}
 	
 }
