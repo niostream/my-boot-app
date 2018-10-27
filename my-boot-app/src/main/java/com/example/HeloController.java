607c18812520e8d566d4c0fdc0174d08b21008b2
Urushibara Tomoya <niostream@gmail.com>
2018-10-27 20:51:34 +0900

    リスト3-13を作成しました。

--- my-boot-app/src/main/java/com/example/HeloController.java
+++ my-boot-app/src/main/java/com/example/HeloController.java
@@ -1,5 +1,14 @@
 package com.example;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HeloController {
+	
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
 	
 }
