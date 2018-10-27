895188c94ce678d9724c2abd2a2584356312d980
Urushibara Tomoya <niostream@gmail.com>
2018-10-27 21:36:25 +0900

    リスト3-22を作成しました。

--- my-boot-app/src/main/java/com/example/HeloController.java
+++ my-boot-app/src/main/java/com/example/HeloController.java
@@ -1,25 +1,24 @@
 package com.example;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HeloController {
 	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String index(Model model) {
-		model.addAttribute("msg", "お名前を書いて送信してください。");
+	@RequestMapping("/")
+	public String index() {
 		return "index";
 	}
 	
-	@RequestMapping(value = "/", method = RequestMethod.POST)
-	public String send(@RequestParam("text1") String str, Model model) {
-		model.addAttribute("msg", "こんにちは、" + str + "さん!");
-		model.addAttribute("value", str);
-		return "index";
+	@RequestMapping("/other")
+	public String other() {
+		return "redirect:/";
+	}
+	
+	@RequestMapping("/home")
+	public String home() {
+		return "forward:/";
 	}
 	
 }
