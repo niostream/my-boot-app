bf9a8848e0fdec19d8f6843f84b9fb181041a0a5
Urushibara Tomoya <niostream@gmail.com>
2018-10-29 21:18:37 +0900

    リスト-15を作成しました。

--- my-boot-app/src/main/java/com/example/HeloController.java
+++ my-boot-app/src/main/java/com/example/HeloController.java
@@ -2,14 +2,18 @@ package com.example;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HeloController {
 	
-	@RequestMapping("/")
-	public String index(Model model) {
-		model.addAttribute("msg", "message 1<hr>message 2<br>message 3");
+	@RequestMapping("/{id}")
+	public String index(@PathVariable int id, Model model) {
+		model.addAttribute("id", id);
+		model.addAttribute("check", id % 2 == 0);
+		model.addAttribute("trueVal", "Even number!");
+		model.addAttribute("falseVal", "Odd number...");
 		return "index";
 	}
 	
