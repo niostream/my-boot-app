ae49d6a2e914940718d31c662f3f57c84363e743
Urushibara Tomoya <niostream@gmail.com>
2018-10-30 21:23:55 +0900

    リスト4-23を作成しました。

--- my-boot-app/src/main/java/com/example/HeloController.java
+++ my-boot-app/src/main/java/com/example/HeloController.java
@@ -4,17 +4,24 @@ import java.util.ArrayList;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HeloController {
 	
-	@RequestMapping("/")
-	public String index(Model model) {
-		ArrayList<String[]> data = new ArrayList<String[]>();
-		data.add(new String[]{"taro", "taro@yamada", "090-999-999"});
-		data.add(new String[]{"hanako", "hanako@flower", "080-888-888"});
-		data.add(new String[]{"sachiko", "sachiko@happy", "080-888-888"});
+	@RequestMapping("/{num}")
+	public String index(@PathVariable int num, Model model) {
+		model.addAttribute("num", num);
+		if (num >= 0) {
+			model.addAttribute("check", "num >= data.size() ? 0 : num");
+		} else {
+			model.addAttribute("check", "num <= data.size() * (-1) ? 0 : num * (-1)");
+		}
+		ArrayList<DataObject> data = new ArrayList<DataObject>();
+		data.add(new DataObject(0, "taro", "taro@yamada"));
+		data.add(new DataObject(1, "hanako", "hanako@flower"));
+		data.add(new DataObject(2, "sachiko", "sachiko@happy"));
 		model.addAttribute("data", data);
 		return "index";
 	}
