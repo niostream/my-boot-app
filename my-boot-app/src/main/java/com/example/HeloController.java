bb51ccb25d4b3e0d069cc347194ebbf1999c3c51
Urushibara Tomoya <niostream@gmail.com>
2018-10-30 21:33:41 +0900

    リスト4-25を作成しました。

--- my-boot-app/src/main/java/com/example/HeloController.java
+++ my-boot-app/src/main/java/com/example/HeloController.java
@@ -4,20 +4,13 @@ import java.util.ArrayList;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HeloController {
 	
-	@RequestMapping("/{num}")
-	public String index(@PathVariable int num, Model model) {
-		model.addAttribute("num", num);
-		if (num >= 0) {
-			model.addAttribute("check", "num >= data.size() ? 0 : num");
-		} else {
-			model.addAttribute("check", "num <= data.size() * (-1) ? 0 : num * (-1)");
-		}
+	@RequestMapping("/")
+	public String index(Model model) {
 		ArrayList<DataObject> data = new ArrayList<DataObject>();
 		data.add(new DataObject(0, "taro", "taro@yamada"));
 		data.add(new DataObject(1, "hanako", "hanako@flower"));
