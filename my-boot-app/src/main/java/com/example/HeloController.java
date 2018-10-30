1bc003d240e02f7b3426d9575a479f330bd875df
Urushibara Tomoya <niostream@gmail.com>
2018-10-30 21:50:59 +0900

    リスト4-27を作成しました。

--- my-boot-app/src/main/java/com/example/HeloController.java
+++ my-boot-app/src/main/java/com/example/HeloController.java
@@ -1,21 +1,16 @@
 package com.example;
 
-import java.util.ArrayList;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HeloController {
 	
-	@RequestMapping("/")
-	public String index(Model model) {
-		ArrayList<DataObject> data = new ArrayList<DataObject>();
-		data.add(new DataObject(0, "taro", "taro@yamada"));
-		data.add(new DataObject(1, "hanako", "hanako@flower"));
-		data.add(new DataObject(2, "sachiko", "sachiko@happy"));
-		model.addAttribute("data", data);
+	@RequestMapping("/{tax}")
+	public String index(@PathVariable int tax, Model model) {
+		model.addAttribute("tax", tax);
 		return "index";
 	}
 	
