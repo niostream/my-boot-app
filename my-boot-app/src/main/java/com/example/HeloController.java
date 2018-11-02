6e434b66af405aee00a39ef6ba99bb36ac2a337f
Urushibara Tomoya <niostream@gmail.com>
2018-11-02 21:39:18 +0900

    リスト5-1を作成しました。

--- my-boot-app/src/main/java/com/example/HeloController.java
+++ my-boot-app/src/main/java/com/example/HeloController.java
@@ -1,25 +1,92 @@
 package com.example;
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.repositories.MyDataRepository;
 
 @Controller
 public class HeloController {
 	
-	@RequestMapping("/")
-	public String index(Model model) {
+	@Autowired
+	MyDataRepository repository;
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index(@ModelAttribute("formModel") MyData myData, Model model) {
+		model.addAttribute("msg", "this is sample content.");
+		List<MyData> list = repository.findAll();
+		model.addAttribute("datalist", list);
 		return "index";
 	}
 	
-	@RequestMapping("/other")
-	public String other() {
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	@Transactional(readOnly = false)
+	public String form(@ModelAttribute("formModel") MyData myData, Model model) {
+		repository.saveAndFlush(myData);
 		return "redirect:/";
 	}
 	
-	@RequestMapping("/home")
-	public String home() {
-		return "forward:/";
+	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+	public String edit(@ModelAttribute MyData myData, @PathVariable long id, Model model) {
+		model.addAttribute("title", "edit mydata.");
+		Optional<MyData> data = repository.findById(id);
+		model.addAttribute("formModel", data.get());
+		return "edit";
+	}
+	
+	@RequestMapping(value = "/edit", method = RequestMethod.POST)
+	public String update(@ModelAttribute MyData myData, Model model) {
+		repository.saveAndFlush(myData);
+		return "redirect:/";
+	}
+	
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public String delete(@PathVariable long id, Model model) {
+		model.addAttribute("title", "delete mydata.");
+		Optional<MyData> data = repository.findById(id);
+		model.addAttribute("formModel", data.get());
+		return "delete";
+	}
+	
+	@RequestMapping(value = "/delete", method = RequestMethod.POST)
+	@Transactional(readOnly = false)
+	public String remove(@RequestParam long id, Model model) {
+		repository.deleteById(id);
+		return "redirect:/";
+	}
+	
+	@PostConstruct
+	public void init() {
+		MyData d1 = new MyData();
+		d1.setName("tuyano");
+		d1.setAge(123);
+		d1.setMail("syoda@tuyano.com");
+		d1.setMemo("this is my data!");
+		repository.saveAndFlush(d1);
+		MyData d2 = new MyData();
+		d2.setName("hanako");
+		d2.setAge(15);
+		d2.setMail("hanako@flower");
+		d2.setMemo("my girl friend.");
+		repository.saveAndFlush(d2);
+		MyData d3 = new MyData();
+		d3.setName("sachiko");
+		d3.setAge(37);
+		d3.setMail("sachiko@happy");
+		d3.setMemo("my work friend...");
+		repository.saveAndFlush(d3);
 	}
 	
 }
---
+++ my-boot-app/src/main/java/com/example/MyData.java
@@ -0,0 +1,44 @@
+package com.example;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "mydata")
+@Data
+public class MyData {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column
+	@NotNull
+	private long id;
+	
+	@Column(length = 50, nullable = false)
+	@NotEmpty
+	private String name;
+	
+	@Column(length = 200, nullable = true)
+	@Email
+	private String mail;
+	
+	@Column(nullable = true)
+	@Min(0)
+	@Max(200)
+	private Integer age;
+	
+	@Column(nullable = true)
+	private String memo;
+
+}
